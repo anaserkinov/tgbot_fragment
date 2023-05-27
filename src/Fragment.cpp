d9f86892474bfedc74d7aa138846b5e08349d272
anas <anasxonerkinjonov@gmail.com>
2023-05-28 02:48:10 +0500

    some changes

---
+++ src/Fragment.cpp
@@ -0,0 +1,10 @@
+#include "Fragment.hpp"
+
+#include <iostream>
+
+Fragment::Fragment() {
+    std::cout << "dsdsdsdsds";
+}
+
+Fragment::~Fragment() {
+}
---
+++ src/Fragment.hpp
@@ -0,0 +1,14 @@
+#ifndef FRAGMENT
+#define FRAGMENT
+
+class Fragment
+{
+private:
+    /* data */
+public:
+    Fragment();
+    ~Fragment();
+};
+
+
+#endif
